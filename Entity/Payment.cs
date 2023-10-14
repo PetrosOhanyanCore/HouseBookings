fa4e356d67d242ca5a4653269eb71ed74fe5e75b
gevorggukasyan24@mail.ru <gevorggukasyan24@mail.ru>
2023-10-14 19:38:06 +0400

    Entity Payment v2

---
+++ Entity/Payment.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entity
+{
+
+    public class Payment
+    {
+        //[Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int PaymentId { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public Currency CurrencyCode { get; set; }
+
+        [Required]
+        [StringLength(16)]
+        public int CardNumber { get; set; }
+
+        [Required]
+        public string CardOwner { get; set; }
+
+        [MaxLength(3)]
+        public short CVV { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public DateTime PaymentDate { get; set; }
+
+        //public int BookingId { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        public Payment()
+        {
+
+        }
+    }
+
+    public enum Currency
+    {
+        USD, // Dollar USA
+        EUR, // Euro
+        GBP, // Funt-Sterling
+        JPY, // Japan Iyen
+                // others
+    }
+}
+
