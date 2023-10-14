a92ae7c7f18de9899764cb6f163dc5335f96ba09
Mariam Yeremyan <mariam.yeremyan777@gmail.com>
2023-10-14 14:38:55 +0400

    Translation

---
+++ Entity/Translation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Translation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string En { get; set; }
+        public string Rus { get; set; }
+        public string Arm { get; set; }
+        
+    }
+    
+}
