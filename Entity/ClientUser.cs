8f63709a05bc8f60b8f41d234342b911b95c0288
Seyranian <108471348+Seyranian@users.noreply.github.com>
2023-10-14 14:09:49 +0400

    Adding entity for Client Userr

---
+++ Entity/ClientUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class ClientUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        //public Residence { get; set; }
+        //public Rating { get; set; }
+        //public Image { get; set; }
+        //public PaymentType { get; set; }
+    }
+}
