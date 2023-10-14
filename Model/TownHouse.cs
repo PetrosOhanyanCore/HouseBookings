4de6dd60f12b1d68cd6724fa9c0ad83488a74f69
Mkrtich Manukyan <mkrtichmanukyan1@gmail.com>
2023-10-14 02:50:21 -0800

    Comersion/TownHouse/Penthouse | Mkrtich

---
+++ Model/TownHouse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class TownHouse
+    {
+        public int Id { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public bool IsFree { get; set; }
+        public int Price { get; set; }
+        public int Square { get; set; }
+        public bool Utilities { get; set; }
+        public bool Parking { get; set; }
+        public bool InFurniture { get; set; }
+    }
+}
