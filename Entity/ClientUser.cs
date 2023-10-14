ee3ed044ebcda2b72d6121b61942ecbcbfbb8958
Seyranian <108471348+Seyranian@users.noreply.github.com>
2023-10-14 14:29:54 +0400

    Changing Properties in CLient USer

--- Entity/ClientUser.cs
+++ Entity/ClientUser.cs
@@ -11,8 +11,8 @@ namespace Entity
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? DeactivationDate { get; set; }
         //public Residence { get; set; }
         //public Rating { get; set; }
         //public Image { get; set; }
