80603eaa2218d5e7b80b5cd050f0b246fad5e2b7
gevorggukasyan24@mail.ru <gevorggukasyan24@mail.ru>
2023-10-14 14:05:51 +0400

    Entity Payment

---
+++ Entity/Payment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+     public class Payment
+        {
+            //[Key]
+            //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int PaymentId { get; set; }
+
+            [Required]
+            public decimal Amount { get; set; }
+
+            [Required]
+            public Currency CurrencyCode { get; set; }
+
+            public decimal Discount { get; set; }
+
+            [Required]
+            [DataType(DataType.DateTime)]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public DateTime PaymentDate { get; set; }
+
+
+            [Required]
+            public int BookingId { get; set; }
+
+            [ForeignKey("BookingId")]
+            public Booking Booking { get; set; }
+
+
+            public Payment()
+            {
+                PaymentDate = DateTime.Now;
+            Amount = Amount - (Discount * Amount / 100);
+            }
+        }
+
+        public enum Currency
+        {
+            USD, // Dollar USA
+            EUR, // Euro
+            GBP, // Funt-Sterling
+            JPY, // Japan Iyen
+                 // others
+        }
+}
+
