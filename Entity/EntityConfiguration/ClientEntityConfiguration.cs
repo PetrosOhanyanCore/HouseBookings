0f70f6858d5595cc7f035ac1f3dbe94261355932
Seyranian <108471348+Seyranian@users.noreply.github.com>
2023-10-16 19:23:59 +0400

    Adding Client Entity Configuration

---
+++ Entity/EntityConfiguration/ClientEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.EntityConfiguration
+{
+    internal class ClientEntityConfiguration : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.ToTable("Client");
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.FirstName).HasMaxLength(50);
+            builder.Property(c => c.LastName).HasMaxLength(50);
+            builder.Property(c => c.MiddleName).HasMaxLength(50);
+        }
+    }
+}
