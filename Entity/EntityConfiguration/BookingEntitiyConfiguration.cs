57ed7a53e46a7183bc432da1e2b24eef04b12d9e
Norayr Sukiasyan <norayr_sukiasyan@edu.aua.am>
2023-10-17 23:47:51 +0400

    Booking Config

---
+++ Entity/EntityConfiguration/BookingEntitiyConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.EntityConfiguration
+{
+    internal class BookingEntitiyConfiguration : IEntityTypeConfiguration<Booking>
+    {
+
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.ToTable("Booking");
+            builder.HasKey(k => k.Id);
+
+            builder.Property(p => p.Note).HasMaxLength(200);
+            builder.HasOne(p => p.Booking).WithOne(p => p.Client).HasForeignKey(p => p.ClientId);
+            builder.HasOne(p =>p.Booking).WithOne(p => p.Apartment).HasForeignKey(p => p.ApartmentId);
+
+
+
+        }
+    }
+}
