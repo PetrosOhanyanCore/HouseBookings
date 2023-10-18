e4b7213e82e98102480b94747cad0c08f163718a
Mkrtich Manukyan <mkrtichmanukyan1@gmail.com>
2023-10-18 07:26:54 -0800

    Not Finsh Add

---
+++ 001_DataLayer/Repository/ScoringRepository.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer.IRepository;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository
+{
+    public class ScoringRepository
+        : RepositoryBase<Scoring>,
+        IScoringRepository
+    {
+        public ScoringRepository(DataBaseContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Scoring> GetAllBuilding()
+        {
+            List<Scoring> models = _context.Scorings
+            .Where(p => p.IsBuilding == true)
+            .ToList();
+
+            return models;
+        }
+    }
+}
