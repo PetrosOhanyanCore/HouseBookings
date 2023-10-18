ef87c908fbdc96babb239dd81828f8e696e8f288
Seyranian <108471348+Seyranian@users.noreply.github.com>
2023-10-18 19:28:18 +0400

    Add Client Entity Configuration, IClientRepositort and ClientRepository

---
+++ 001_DataLayer/IRepository/IClientRepository.cs
@@ -0,0 +1,17 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.IRepository
+{
+    public interface IClientRepository : IRepositoryBase<Client>
+    {
+        Client GetCLientByID(int id);
+        Task<Client> GetClientByIdAsync(int id);
+        IEnumerable<Client> GetAllClients();
+
+    }
+}
