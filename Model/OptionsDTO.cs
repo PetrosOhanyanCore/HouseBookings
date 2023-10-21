056186e0ea4bcb57d4a6a58ce565916a70f51c02
Mkrtich Manukyan <mkrtichmanukyan1@gmail.com>
2023-10-21 02:44:24 -0800

    OptionsDTO

---
+++ Model/OptionsDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class OptionsDTO
+    {
+        public int Id { get; set; }
+        public ApartmentDTO Apartment { get; set; }
+        public BuildingDTO Building { get; set; }
+        public string Discription { get; set; }
+    }
+}
