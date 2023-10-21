206452731b1a89d2731f836bcc56b43472017432
Mkrtich Manukyan <mkrtichmanukyan1@gmail.com>
2023-10-21 22:55:49 +0400

    Add all

--- HouseBooking/Program.cs
+++ HouseBooking/Program.cs
@@ -14,9 +14,11 @@ options.UseSqlServer(
 // Add services to the container.
 builder.Services.AddScoped<IApartmentRepository, ApartmentRepository>();
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
+builder.Services.AddScoped<IOptionsRepository, OptionsRepository>();
 
 
 builder.Services.AddTransient<IApartmentService, ApartmentService>();
+builder.Services.AddTransient<IOptionsService, OptionsService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
