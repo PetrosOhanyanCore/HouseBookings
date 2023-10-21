34c1c9c769f598adf7fb6b83f8834c1810409dbf
Mkrtich Manukyan <mkrtichmanukyan1@gmail.com>
2023-10-21 22:55:49 +0400

    Add all

--- Model/Mapper/OptionsProfile.cs
+++ Model/Mapper/OptionsProfile.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AutoMapper;
+using Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Model.Mapper
 {
-    internal class OptionsProfile
+    public class OptionsProfile : Profile
     {
+        OptionsProfile()
+        {
+            CreateMap<OptionsDTO, Options>();
+
+            CreateMap<Options, OptionsDTO>();
+        }
     }
 }
