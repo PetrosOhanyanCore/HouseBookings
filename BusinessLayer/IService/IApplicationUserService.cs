dc0e404dc7064fd51995b47cee5651f6c6fea9ad
Seyranian <108471348+Seyranian@users.noreply.github.com>
2023-10-23 16:14:46 +0400

    Add IApplicationUserService,ApplicationUserService, ApplicationUSerDTO and ApplicationUSerProfile

---
+++ BusinessLayer/IService/IApplicationUserService.cs
@@ -0,0 +1,28 @@
+﻿using Entity;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.IService
+{
+    public interface IApplicationUserService
+    {
+        Task<IEnumerable<ApplicationUser>> GetAllApplicationUsersInFirstNameAsync(string firstName);
+        Task<IEnumerable<ApplicationUser>> GetAllApplicationUsersNotInLastNameAsync(string lastName);
+        Task<IEnumerable<ApplicationUser>> GetAllActiveUsersAsync();
+        Task<int> CountAllActiveUsersAsync();
+        Task<IEnumerable<ApplicationUser>> GetAllApplicationUsersInCreatedTimeAsync(DateTime createdTime);
+        Task<IEnumerable<ApplicationUser>> GetAllApplicationUSersInDeactivatedAsync(DateTime deactivatedTime);
+        Task<ApplicationUser> GetApplicationUserInClientIdAsync(int clientId);
+        Task<ApplicationUser> GetApplicationUserInPhoneNumberAsync(string phoneNumber);
+        Task<IEnumerable<ApplicationUser>> GetAllApplicationUSersByAccesTokenAsync(string accesToken);
+        Task<IEnumerable<ApplicationUser>> GetAllApplicationUsersByBirthDateAsync(DateTime birthDate);
+        void CreateApplicationUserAsync(ApplicationUserDTO applicationUserDTO);
+        void UpdateApplicationUserAsync(ApplicationUserDTO applicationUserDTO);
+        void DeleteApplicationUserAsync(ApplicationUserDTO applicationUserDTO);
+    }
+}
--- HouseBooking/Program.cs
+++ HouseBooking/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using DataLayer.Repository;
 using BusinessLayer.IService;
 using BusinessLayer.Service;
+using Entity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,7 @@ builder.Services.AddScoped<IClientRepository, ClientRepository>();
 
 
 builder.Services.AddTransient<IApartmentService, ApartmentService>();
+builder.Services.AddTransient<IApplicationUserService, ApplicationUserService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
