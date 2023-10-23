16139ad55cc9d54070c12105926d9c0bab810eca
Samvel Danielyan <samvel13danielyan@gmail.com>
2023-10-23 05:31:35 -0800

    Image

---
+++ 001_DataLayer/IRepository/IImageRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using DataLayer.IRepository;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.IRepository
+{
+    public interface IImageRepository : IRepositoryBase<Image>
+    {
+        Image GetImageByID(int id);
+        Task<Image> GetImageByIdAsync(int id);
+
+        IEnumerable<Image> GetAllImages(int buildingId);
+    }
+}
