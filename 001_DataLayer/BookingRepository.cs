1736502da3e66714f06a92c9e0541f5a96dafaba
Sargis Msryan <sargis.msryan@imaginelive.com>
2023-10-25 17:34:59 +0400

    BookingRepository

---
+++ 001_DataLayer/BookingRepository.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer.IRepository;
+using DataLayer.Repository;
+using Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class BookingRepository : RepositoryBase<Booking>
+    {
+        public BookingRepository(DataBaseContext context) : base(context)
+        {
+        }
+
+
+
+        public async Task<Booking> GetBuildingByApartmentIdAsync(int id)
+        {
+            var result = await _context.Addresses.FirstOrDefaultAsync(i => i.Id == id);
+            return result;
+        }
+    }
+}
