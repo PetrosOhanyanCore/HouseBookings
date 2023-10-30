a6a00f4074af63251f6f0fd7a6b5c6183260516f
Norayr Sukiasyan <norayr_sukiasyan@edu.aua.am>
2023-10-30 19:26:23 +0400

    new

---
+++ HouseBooking/Controllers/BuildingImageController.cs
@@ -0,0 +1,72 @@
+﻿using BusinessLayer.IService;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace HouseBooking.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuildingImageController
+    {
+        public IBuildingImageService _service;
+
+        public BuildingImageController(IBuildingImageService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("GetBuildingImageByIdAsync/{id}")]
+        public async Task<BuildingImageDTO> GetBuildingImageByIdAsync(int id)
+        {
+           var result = await _service.GetBuildingImageByIdAsync(id);
+            return result;
+        }
+
+        [HttpGet("GetBuildingAllImagesAsync/{buildingId}")]
+        public async Task<ICollection<BuildingImageDTO>> GetBuildingAllImagesAsync(int buildingId)
+        {
+            return await _service.GetBuildingAllImagesAsync(buildingId);
+        }
+
+
+        [HttpPost("AddBuildingImage/{buildingImageDTO}")]
+        public void AddBuildingImage(BuildingImageDTO buildingImageDTO)
+        {
+            _service.AddBuildingImage(buildingImageDTO);
+        }
+
+
+        [HttpPost("AddRangeBuildingImages/{buildingImageDTOs}")]
+        public void AddRangeBuildingImages(ICollection<BuildingImageDTO> buildingImageDTOs)
+        {
+            _service.AddRangeBuildingImages(buildingImageDTOs);
+           
+        }
+
+
+        [HttpPut("UpdateBuildingImage/{buildingImageDTO}")]
+        public void UpdateBuildingImage(BuildingImageDTO buildingImageDTO)
+        {
+            _service.UpdateBuildingImage(buildingImageDTO); 
+        }
+
+        [HttpPut("UpdateRangeBuildingImages/{buildingImageDTOs}")]
+        public void UpdateRangeBuildingImages(ICollection<BuildingImageDTO> buildingImageDTOs)
+        {
+            _service.UpdateRangeBuildingImages(buildingImageDTOs);
+        }
+
+        [HttpDelete("RemoveBuildingImage/{buildingImageDTO}")]
+        public void RemoveBuildingImage(BuildingImageDTO buildingImageDTO)
+        {
+            _service.RemoveBuildingImage(buildingImageDTO); 
+        }
+
+        [HttpDelete("RemoveRangeBuildingImages/{buildingImageDTOs}")]
+        public void RemoveRangeBuildingImages(ICollection<BuildingImageDTO> buildingImageDTOs)
+        {
+            _service.RemoveRangeBuildingImages(buildingImageDTOs);
+        }
+    }
+}
