c13e9c60103a3799ee3455085721129c2c119c40
Samvel Danielyan <samvel13danielyan@gmail.com>
2023-10-31 21:34:25 +0400

    TranslationController

---
+++ HouseBooking/Controllers/TranslationController.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLayer.IService;
+using BusinessLayer.Service;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+
+namespace HouseBooking.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslationController : ControllerBase
+    {
+        ITranslationService _translationService;
+        public TranslationController(ITranslationService translationService)
+        {
+            this._translationService = translationService;
+        }
+
+        [HttpGet("SwithLanguage")]
+        public int SwithLanguage(string language)
+        {
+            int translation = _translationService.SwithLanguage(language);
+
+            if (translation != null)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
+}
+
+
