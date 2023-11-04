35a9efad6c2887865966c3f31b389bd4239e09d3
vahesanosyan <vahesanosyan@gmail.com>
2023-11-04 13:54:59 +0400

    BuildingControllerNew

--- BusinessLayer/Service/BuildingService.cs
+++ BusinessLayer/Service/BuildingService.cs
@@ -6,6 +6,7 @@ using Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,6 +23,25 @@ namespace BusinessLayer.Service
             _mapper = maper;
         }
 
+        public void Add(BuildingDTO buildingdto)
+        {
+            Building building = _mapper.Map<Building>(buildingdto) ;
+            _buildingRepositroy.Add(building);
+        }
+
+        public void AddRange(IEnumerable<BuildingDTO> buildingdtos)
+        {
+            List<Building> list = new List<Building>();
+
+            foreach (var item in buildingdtos)
+            {
+                var dto = _mapper.Map<Building>(item);
+                list.Add(dto);
+            }
+
+            _buildingRepositroy.AddRange(list);
+        }
+
         public IEnumerable<BuildingDTO> GetAllBuildings(int locationId)
         {
             var buildings = (List<BuildingDTO>)_buildingRepositroy.GetAllBuildings(locationId);
@@ -29,6 +49,8 @@ namespace BusinessLayer.Service
             return buildings;
 
         }
+
+        
 
         public async Task<BuildingDTO> GetBuildingById(int buildingId)
         {
@@ -41,6 +63,42 @@ namespace BusinessLayer.Service
         public int ImagesCountByImageId(int id)
         {
             return _buildingRepositroy.ImagesCountByImageId(id);
+        }
+
+        public void Remove(BuildingDTO entity)
+        {
+            var building = _mapper.Map<Building>(entity);
+            _buildingRepositroy.Remove(building);
+        }
+
+        public void RemoveRange(IEnumerable<BuildingDTO> entities)
+        {
+            List<Building> list = new List<Building>();
+            foreach (var entity in entities)
+            {
+                var dto = _mapper.Map<Building>(entity);
+                list.Add(dto);
+            }
+            _buildingRepositroy.RemoveRange(list);
+        }
+        
+       
+
+        public void Update(BuildingDTO entity)
+        {
+            var building= _mapper.Map<Building>(entity);
+            _buildingRepositroy.Update(building);
+        }
+
+        public void UpdateRange(IEnumerable<BuildingDTO> entities)
+        {
+            List<Building> list = new List<Building>();
+            foreach(var entity in entities)
+            {
+                var dto= _mapper.Map<Building>(entity);
+                list.Add(dto);
+            }
+            _buildingRepositroy.UpdateRange(list);
         }
     }
 }
